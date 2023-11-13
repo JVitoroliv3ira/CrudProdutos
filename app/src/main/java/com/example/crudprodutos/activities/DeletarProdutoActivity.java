49801d4b00490606f2cb36ac5007867a94fc3292
jvitor07 <jvitoroliveira0711@gmail.com>
2023-11-13 18:10:16 -0300

    Implementa logica de limpar o campo do codigo na tela de deletar

--- app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
+++ app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.crudprodutos.R;
 
 public class DeletarProdutoActivity extends AppCompatActivity {
+
+    private EditText editTextCodigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deletar_produto);
 
+        this.editTextCodigo = findViewById(R.id.edit_text_deletar_codigo);
+
+        Button botaoLimpar = findViewById(R.id.deletar_produto_limpar);
+        botaoLimpar.setOnClickListener(v -> this.handleLimpar());
+
         Button botaoVoltar = findViewById(R.id.deletar_produto_voltar);
         botaoVoltar.setOnClickListener(v -> this.handleVoltar());
+    }
+
+    private void handleLimpar() {
+        this.editTextCodigo.setText("");
     }
 
     private void handleVoltar() {
