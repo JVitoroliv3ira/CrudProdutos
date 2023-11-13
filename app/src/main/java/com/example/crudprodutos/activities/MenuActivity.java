461b6b9ff562d35f5f1e31b6bfe9b853cc3bdca2
jvitor07 <jvitoroliveira0711@gmail.com>
2023-11-13 17:36:54 -0300

    Adiciona acoes nos botoes do menu

--- app/src/main/java/com/example/crudprodutos/activities/MenuActivity.java
+++ app/src/main/java/com/example/crudprodutos/activities/MenuActivity.java
@@ -17,10 +17,34 @@ public class MenuActivity extends AppCompatActivity {
 
         Button cadastrarProdutoButton = findViewById(R.id.cadastrar_produto);
         cadastrarProdutoButton.setOnClickListener(v -> this.irParaCadastrarProdutos());
+
+        Button editarProdutoButton = findViewById(R.id.editar_produto);
+        editarProdutoButton.setOnClickListener(v -> this.irParaEditarProduto());
+
+        Button listarProdutosButton = findViewById(R.id.listar_produtos);
+        listarProdutosButton.setOnClickListener(v -> this.irParaListarProdutos());
+
+        Button deletarProdutoButton = findViewById(R.id.deletar_produto);
+        deletarProdutoButton.setOnClickListener(v -> this.irParaDeletarProduto());
     }
 
     private void irParaCadastrarProdutos() {
         Intent intent = new Intent(this, CadastrarProdutoActivity.class);
+        startActivity(intent);
+    }
+
+    private void irParaEditarProduto() {
+        Intent intent = new Intent(this, EditarProdutoActivity.class);
+        startActivity(intent);
+    }
+
+    private void irParaListarProdutos() {
+        Intent intent = new Intent(this, ListarProdutosActivity.class);
+        startActivity(intent);
+    }
+
+    private void irParaDeletarProduto() {
+        Intent intent = new Intent(this, DeletarProdutoActivity.class);
         startActivity(intent);
     }
 }
