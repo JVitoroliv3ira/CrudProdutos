d436b9916a2ed928aa78cdd659726b2f35f99cc4
jvitor07 <jvitoroliveira0711@gmail.com>
2023-11-13 18:14:20 -0300

    Implementa boa parte da logica de deletar produto

--- app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
+++ app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
@@ -1,12 +1,15 @@
 package com.example.crudprodutos.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.crudprodutos.R;
+
+import java.util.Objects;
 
 public class DeletarProdutoActivity extends AppCompatActivity {
 
@@ -19,11 +22,33 @@ public class DeletarProdutoActivity extends AppCompatActivity {
 
         this.editTextCodigo = findViewById(R.id.edit_text_deletar_codigo);
 
+        Button botaoCadastrar = findViewById(R.id.deletar_produto_deletar);
+        botaoCadastrar.setOnClickListener(v -> {
+            String codigo = editTextCodigo.getText().toString().isEmpty() ? "" : editTextCodigo.getText().toString();
+            this.handleDeletar(codigo);
+        });
+
         Button botaoLimpar = findViewById(R.id.deletar_produto_limpar);
         botaoLimpar.setOnClickListener(v -> this.handleLimpar());
 
         Button botaoVoltar = findViewById(R.id.deletar_produto_voltar);
         botaoVoltar.setOnClickListener(v -> this.handleVoltar());
+    }
+
+    private void handleDeletar(String codigo) {
+        if (Boolean.TRUE.equals(this.validarFormulario(codigo))) {
+            this.finish();
+            Toast.makeText(getApplicationContext(), "Produto deletado com sucesso.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Boolean validarFormulario(String codigo) {
+        if (Boolean.TRUE.equals(Objects.isNull(codigo) || codigo.isEmpty())) {
+            Toast.makeText(getApplicationContext(), "Código do produto está vazio ou nulo.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
     }
 
     private void handleLimpar() {
