1cc571eb686a51290c5701dd9132b5bbf6a4b76a
jvitor07 <jvitoroliveira0711@gmail.com>
2023-11-13 18:02:04 -0300

    wip

--- app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
+++ app/src/main/java/com/example/crudprodutos/activities/DeletarProdutoActivity.java
@@ -3,6 +3,7 @@ package com.example.crudprodutos.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.crudprodutos.R;
 
@@ -12,5 +13,12 @@ public class DeletarProdutoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deletar_produto);
+
+        Button botaoVoltar = findViewById(R.id.deletar_produto_voltar);
+        botaoVoltar.setOnClickListener(v -> this.handleVoltar());
+    }
+
+    private void handleVoltar() {
+        this.finish();
     }
 }
